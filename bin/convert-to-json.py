107f9421bcc8f17d0988b06f7fd3b7435a958613
ArinKothari <96499331+ArinKothari@users.noreply.github.com>
2023-07-21 16:29:12 +0530

    Update convert-to-json.py

--- bin/convert-to-json.py
+++ bin/convert-to-json.py
@@ -11,4 +11,6 @@ if len(sys.argv) < 2:
     sys.stderr.write('Usage: make-data.py FILENAME')
     sys.exit(1)
 
-print(json.dumps(utils.import_data(open(sys.argv[1])), indent=4))
+json_object = json.dumps(utils.import_data(open(sys.argv[1])), indent=4)
+with open("tmp/output.txt", "w") as outfile:
+  outfile.write(json_object)
